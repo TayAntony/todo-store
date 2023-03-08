26a959c08e94ab03eeb9598928b9030000120164
Tayssa Antoniasse <98670493+TayAntony@users.noreply.github.com>
2023-03-08 16:53:55 -0300

    favicon add
    tentar centralizar os componentes na tela

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,26 @@
 import Logo from "./assets/logo.png"
-import './App.css'
 
 function App() {
 
   return (
     <div className="App imgBackground">
-      <div className='absolute'>
-        <h1 className='todo'>ToDo</h1>
-        <p className='slogan'>Onde você faz acontecer!</p>
-      </div>
+      
+      <div className="maximo">
+        <div className='absolute'>
+          <h1 className='todo'>ToDo</h1>
+          <p className='slogan'>Onde você faz acontecer!</p>
+        </div>
 
-      <div className="borda">
-        <div className='cardInicio'>
-          <img src={Logo} alt="" className="logo"/>
-          <p>Seja bem vindo(a) ao ToDo, a minha plataforma de E-commerce e digital bank!</p>
-          <button className="mainButton">Sou novo!</button>
-          <a href="#" className="possuiConta">Já possui uma conta?</a>
+        <div className="borda">
+          <div className='cardInicio'>
+            <img src={Logo} alt="" className="logo"/>
+            <p>Seja bem vindo(a) ao ToDo, a minha plataforma de E-commerce e digital bank!</p>
+            <button className="mainButton">Sou novo!</button>
+            <a href="#" className="possuiConta">Já possui uma conta?</a>
+          </div>
         </div>
       </div>
+
     </div>
   )
 }
